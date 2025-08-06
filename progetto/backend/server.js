474c998fcac90d8ee0455a4190e82b0ac77ebd89
Carlo Ganci <carloganci98@gmail.com>
2025-08-06 10:47:43 +0200

    aggiunto filtro per gusto

--- progetto/backend/server.js
+++ progetto/backend/server.js
@@ -5,49 +5,71 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Il server è attivo");
-})
+  res.send("Il server è attivo");
+});
 
 app.get("/aziende", (req, res) => {
-    res.json(database);
-})
+  res.json(database);
+});
 
 app.get("/aziende/:id", (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    // quello che prendiamo dall'url è sempre una stringa, quindi se devo confrontare stringhe mettere sempre toLowerCase, se devo confrontare un parametro numerico con stringa, o metto parseInt oppure == invece di === (soluzione meno elegante).
+  // quello che prendiamo dall'url è sempre una stringa, quindi se devo confrontare stringhe mettere sempre toLowerCase, se devo confrontare un parametro numerico con stringa, o metto parseInt oppure == invece di === (soluzione meno elegante).
 
-    const azienda = database.find((x) => x.id === parseInt(id));
-    if (azienda) {
-        res.json(azienda);
-    } else {
-        res.status(404).json({error: "Azienda non trovata"})
-    }
-    
-})
+  const azienda = database.find((x) => x.id === parseInt(id));
+  if (azienda) {
+    res.json(azienda);
+  } else {
+    res.status(404).json({ error: "Azienda non trovata" });
+  }
+});
 
-app.get("/aziende/origine/:origine",(req, res)=>{
-const {origine}= req.params;
-const origini= database.filter((x)=> x.origine.toLowerCase().trim() === origine.toLowerCase().trim() );
-if(origini.length > 0){
+app.get("/aziende/origine/:origine", (req, res) => {
+  const { origine } = req.params;
+  const origini = database.filter(
+    (x) => x.origine.toLowerCase().trim() === origine.toLowerCase().trim()
+  );
+  if (origini.length > 0) {
     res.json(origini);
-}else{
-    res.status(404).json({error: "Nessun risultato trovato"});
-}
-}
+  } else {
+    res.status(404).json({ error: "Nessun risultato trovato" });
+  }
+});
 
-)
+app.get("/disponibili", (req, res) => {
+  const aziendeFiltrate = database.filter((x) => {
+    const verifica = x.prodotti.some((y) => y.disponibilita);
+    console.log(verifica);
+    return verifica;
+  });
+  if (aziendeFiltrate.length > 0) {
+    res.json(aziendeFiltrate);
+  } else {
+    res.status(404).json({ error: "Nessun risultato trovato" });
+  }
+});
 
-app.get ("/disponibili", (req, res) => {
-    const aziendeFiltrate = database.filter((x) => {const verifica = x.prodotti.some((y) => y.disponibilita) 
-        console.log (verifica)
-        return verifica
-    })
-    if(aziendeFiltrate.length > 0){
-        res.json(aziendeFiltrate);
-    }else{
-        res.status(404).json({error: "Nessun risultato trovato"});
+// richiesta get che restituisca tramite req.query tutte le aziende che producano il gusto di gelato, categoria gelato, che ho indicato nelle query. Se non e' presente restituisce tutte le aziende.
+
+app.get("/gusti", (req, res) => {
+  const { gusto } = req.query;
+  if (gusto) {
+    const aziendeGelato = database.filter((x) =>
+      x.prodotti.some(
+        (y) => y.categoria === "gelato" && y.gusti.includes(gusto.toLowerCase())
+      )
+    );
+    if (aziendeGelato.length > 0) {
+      res.json(aziendeGelato);
+    } else {
+      res.status(404).json({ error: "Nessun gusto trovato" });
     }
-})
+  } else {
+    res.json(database);
+  }
+});
 
-app.listen(PORT, () => console.log(`Il server è attivo su http://localhost:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Il server è attivo su http://localhost:${PORT}`)
+);
